7af4fd9e981be3480ae3d1230011e507f7906533
fawadik <fawadik@yahoo.com>
2021-12-17 20:46:26 +0100

    Home component improved, there is still an error

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Typical from "react-typical";
 
 function Home() {
   return (
@@ -6,18 +7,33 @@ function Home() {
       <div className="home-parent">
         <div className="home-details">
           <div className="columns">
-            <a href="#">
+            <a href="https://www.facebook.com/fowad.latif">
               <i className="fa fa-facebook-square"></i>
             </a>
-            <a href="#">
-              <i className="fa fa-github"></i>
+            <a href="https://github.com/fawadik">
+              <i className="fa fa-github-square"></i>
             </a>
-            <a href="#">
-              <i className="fa fa-linkedin"></i>
+            <a href="https://www.linkedin.com/in/fowad-latif/">
+              <i className="fa fa-linkedin-square"></i>
             </a>
-            <a href="#">
-              <i className="fa fa-instagram"></i>
+            <a href="https://twitter.com/fawadik99">
+              <i className="fa fa-twitter-square"></i>
             </a>
+          </div>
+          <div className="home-details-name">
+            <span className="primary-text">
+              {" "}
+              Hey, I am <span className="highlighted-text">Fowad</span>
+            </span>
+          </div>
+          <div className="home-details-role">
+            <span className="primary-text">
+              {" "}
+              <h1>
+                {" "}
+                <Typical />
+              </h1>
+            </span>
           </div>
         </div>
       </div>
