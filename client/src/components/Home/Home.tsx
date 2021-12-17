a2b6a00bf76e29451fee7b8b246c82a81fd990a4
fawadik <fawadik@yahoo.com>
2021-12-17 19:32:08 +0100

    Home component written

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
-    return (
-        <div>
-            
+  return (
+    <div className="home-container">
+      <div className="home-parent">
+        <div className="home-details">
+          <div className="columns">
+            <a href="#">
+              <i className="fa fa-facebook-square"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-github"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-linkedin"></i>
+            </a>
+            <a href="#">
+              <i className="fa fa-instagram"></i>
+            </a>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
